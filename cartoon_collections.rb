bb412098619755d6e2b7d5fa5f0886db370da4ca
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 22:29:55 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -9,6 +9,7 @@ end
 def summon_captain_planet(planeteer_calls)
   array.map do |element|
     element.uppercase
+  end
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
   # Return a new array of the results
 end
