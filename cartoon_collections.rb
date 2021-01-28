120743a7d2b75d1c3f633aa3be98e37745c03dfe
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 22:40:16 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def square_array(array)
 end
 
 def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map do |string| string.capitalize
+  planeteer_calls.map do |string| string.capitalized
  "#{string}!"
   end
   # Use an Enumerable to capitalize and add '!' to every element in the passed in array
