464a8dbab01a07b9a2a32c3d567df1ae0f6183e4
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 23:39:10 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,8 +24,8 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-  planeteer_calls.any? do |string|
-"#{string}".include?(valid_calls)
+  planeteer_calls.any? do {|string| strings.lenght}
+"#{string}".include?
 end
   # Use an Enumerable to check if any elements in the passed in array match the valid calls listed above
   # Return the first valid call found, or return nil if no valid calls are found
