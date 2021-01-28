a3e19b57a12a9d4e5ae2dc5ccc9163ec906dac1e
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 23:48:45 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -24,7 +24,7 @@ end
 
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
-planeteer_calls.find do |string|.include?
+planeteer_calls.find do |string|
 if valid_calls.true
   
 end
