5db29658cd0fb1df0edbcbde7b741cc77cc85eb9
Mario Puerto <mariopuerto.nyc@gmail.com>
2021-01-28 23:01:15 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -18,8 +18,8 @@ def long_planeteer_calls(planeteer_calls)
   planeteer_calls.all? do |string| string.length < 4
 if planeteer_calls.length < 4
   
-  
-    
+
+
   end
 
   # Use an Enumerable to check if any string in the passed in array is greater than 4 characters long
